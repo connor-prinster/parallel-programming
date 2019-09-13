64349698da398e7878ef65994e61ba6745e58101
Connor Prinster <connor.prinster@gmail.com>
2019-09-13 12:50:13 -0600

    begin hw3: integer sort

---
+++ integerSort/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <mpi.h>
+#include <stdio.h>
+
+#define MCW MPI_COMM_WORLD
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	int rank, size;
+	int data;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MCW, &rank);
+	MPI_Comm_size(MCW, &size);
+
+
+	MPI_Finalize();
+
+	return 0;
+}
