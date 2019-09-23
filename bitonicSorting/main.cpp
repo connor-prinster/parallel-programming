b66bc12b0899b7a42bff22c0485871cc132e0b7b
Connor Prinster <connor.prinster@gmail.com>
2019-09-22 20:56:34 -0600

    added a sort function

--- bitonicSorting/main.cpp
+++ bitonicSorting/main.cpp
@@ -3,26 +3,42 @@
 #include <time.h>
 #include <stdio.h>      /* printf */
 #include <stdlib.h>     /* qsort */
-#include <vector>
 
 #define MCW MPI_COMM_WORLD
 
 using namespace std;
+
+void printArr(int arr[], int randLength);
 
 int main(int argc, char **argv) {
 	srand(time(NULL)); // init the time
 
 	int rank, size;
 	int* list = NULL;
-	int randomLength, perProc;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MCW, &rank);
 	MPI_Comm_size(MCW, &size);
 
-	printf("hello\n");
+	if(rank == 0) {
+		int len = (rand() % 100 + 1) * (size - 1);
+		list = (int*) malloc(len * sizeof(int));
+		for(int i = 0; i < len; i++) {
+			list[i] = rand() % 500 + 1;
+		}
+		printArr(list, len);
+	}
 
 	MPI_Finalize();
 
 	return 0;
+}
+
+void printArr(int* arr, int randLength) {
+	if(arr != NULL) {
+		for(int i = 0; i < randLength; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n\n\n");
+	}
 }
