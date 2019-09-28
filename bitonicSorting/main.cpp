7a918bac342e7836a914f0136ebd5e6da5c4dd44
Connor Prinster <connor.prinster@gmail.com>
2019-09-27 18:40:48 -0600

    now will attempt to get to bitonic situation

--- bitonicSorting/main.cpp
+++ bitonicSorting/main.cpp
@@ -39,8 +39,8 @@ int main(int argc, char **argv) {
 	int dmask = mask;
 
 	if(rank == 0) {
-//		int fakeList[powSize] = {4, 3, 0, 7, 1, 6, 2, 5};
-		int fakeList[powSize] = {2, 4, 6, 8, 7, 5, 3, 1};
+		int fakeList[powSize] = {4, 3, 0, 7, 1, 6, 2, 5};
+//		int fakeList[powSize] = {2, 4, 6, 8, 7, 5, 3, 1};
 		list = (int*) malloc(powSize * sizeof(int));
 		for(int i = 0; i < powSize; i++) {
 			list[i] = fakeList[i];
@@ -54,36 +54,37 @@ int main(int argc, char **argv) {
 	int val = list[rank]; // the initial value will be the spot in the array
 	int dest = 0; //init destination is initialized
 	int recv = 0; // receive value is initialized here
-	//mask = 1; // mask will be at 1 to begin
-	//int phase = 0; // start at phase 0
-	//while(phase < (powOfTwo - 1)) { // go through (n - 1) times (with n being the 2^n multiplier)
-	//	mask = 1; // get mask into it's original value
-	//	mask <<= phase; // mask is shifted by phase (1 -> 1, 1 -> 2, 1 -> 4, etc)
-	//	while(mask > 0) {
-	//		dest = rank ^ mask; // xor to get the destination
-	//		MPI_Send(&val, 1, MPI_INT, dest, 0, MCW);
-	//		MPI_Recv(&recv, 1, MPI_INT, dest, 0, MCW, MPI_STATUS_IGNORE);
-	//		int cending = !((rank & (mask << 1)) == 0); // this will be whether or not the spot will be in/decreasing
-	//		val = ascDesc(cending, rank, mask, val, recv); // the value to be kept is reliant on cending status
-	//		mask >>= 1; // decrease the mask by a power of two
-	//	}
-	//	// phase x is finished, move to phase x + 1;
-	//	phase++;
-	//}
-	//MPI_Barrier(MCW);
-	//printf("\nrank: %d -> val: %d\n", rank, val);
-
-	recv = 0;
-	dmask = mask >> 1;
-	dest = (rank ^ dmask);
-	while(dmask > 0) {
-		dest = (rank ^ dmask);
-		MPI_Send(&val, 1, MPI_INT, dest, 0, MCW);
-		MPI_Recv(&recv, 1, MPI_INT, dest, 0, MCW, MPI_STATUS_IGNORE);
-		val = ascDesc(0, rank, dmask, val, recv);
-		dmask >>= 1;
+	mask = 1; // mask will be at 1 to begin
+	int phase = 0; // start at phase 0
+	while(phase < (powOfTwo - 1)) { // go through (n - 1) times (with n being the 2^n multiplier)
+		mask = 1; // get mask into it's original value
+		mask <<= phase; // mask is shifted by phase (1 -> 1, 1 -> 2, 1 -> 4, etc)
+		while(mask > 0) {
+			dest = rank ^ mask; // xor to get the destination
+			MPI_Send(&val, 1, MPI_INT, dest, 0, MCW);
+			MPI_Recv(&recv, 1, MPI_INT, dest, 0, MCW, MPI_STATUS_IGNORE);
+			int cending = !((rank & (mask << 1)) == 0); // this will be whether or not the spot will be in/decreasing
+			val = ascDesc(cending, rank, mask, val, recv); // the value to be kept is reliant on cending status
+			mask >>= 1; // decrease the mask by a power of two
+		}
+		// phase x is finished, move to phase x + 1;
+		phase++;
 	}
+	MPI_Barrier(MCW);
 	printf("\nrank: %d -> val: %d\n", rank, val);
+
+	//mask = size;
+	//recv = 0;
+	//dmask = mask >> 1;
+	//dest = (rank ^ dmask);
+	//while(dmask > 0) {
+	//	dest = (rank ^ dmask);
+	//	MPI_Send(&val, 1, MPI_INT, dest, 0, MCW);
+	//	MPI_Recv(&recv, 1, MPI_INT, dest, 0, MCW, MPI_STATUS_IGNORE);
+	//	val = ascDesc(0, rank, dmask, val, recv);
+	//	dmask >>= 1;
+	//}
+	//printf("\nrank: %d -> val: %d\n", rank, val);
 
 	MPI_Finalize();
 	return 0;
