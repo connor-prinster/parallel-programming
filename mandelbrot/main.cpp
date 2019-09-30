0e5cbe429e27109e9972e6f7e0046875825e5d8d
Connor Prinster <connor.prinster@gmail.com>
2019-09-30 15:04:42 -0600

    added the timer
    has a mandelbrot image now
    
    fixed a little bit
    
    maybe close?
    
    work and hopefully works on pi
    
    seeing the ppm

--- mandelbrot/main.cpp
+++ mandelbrot/main.cpp
@@ -1,33 +1,100 @@
 #include <iostream>
-#include <mpi.h>
 #include <stdio.h>
+#include <chrono>
+#include <ctime>
+#include <string>
+#include <complex>
+#include <fstream>
 
 #define MCW MPI_COMM_WORLD
 
 using namespace std;
 
-int main(int argc, char **argv) {
-	if( argc < 4 ) {
+struct Color
+{
+	int red;
+	int green;
+	int blue;
+};
+
+int width = 512;
+int height = 512;
+
+Color generateValue(complex<double> complex);
+
+int main(int argc, char** argv) {
+	auto start = std::chrono::system_clock::now();
+
+	if (argc < 4) {
 		printf("\nNot enough arguments. Requires 3 doubles as arguments\n");
-		return (1);
+		return -1;
+	}
+	double r0 = atof(argv[1]);
+	double i0 = atof(argv[2]);
+	double r1 = atof(argv[3]);
+	double i1 = i0 + (r1 - r0);
+
+	// check to see if you can even open the ppm file
+	ofstream mandel("brot.ppm");
+	if (!mandel.is_open()) {
+		printf("you failed, we'll get 'em next time\n\n");
+		return -1;
 	}
 
-	int rank, size;
-	int data;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MCW, &rank);
-	MPI_Comm_size(MCW, &size);
+	// fill in the file
+	mandel << "P3\n" << width << " " << height << " 255\n"; // this particular line is for the header
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			std::complex<double> complex(
+				r0 + (j * (r1 - r0) / height),
+				i0 + (i * (i1 - i0) / width)
+			);
+			Color val = generateValue(complex);
+			mandel << val.red << " " << val.blue << " " << val.green << "\n";
+		}
+		mandel << "\n";
+	}
+	mandel.close();
 
-	double Re0 = atof(argv[1]);
-	double lm0 = atof(argv[2]);
-	double Re1 = atof(argv[3]);
-
-	MPI_Send(&rank, 1, MPI_INT, (rank + 1) % size, 0, MCW);
-	MPI_Recv(&data, 1, MPI_INT, MPI_ANY_SOURCE, 0, MCW, MPI_STATUS_IGNORE);
-
-//	cout << "I am " << rank << " of " << size << "; got message from " << data << endl;
-
-	MPI_Finalize();
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+	std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
 	return 0;
+}
+
+Color generateValue(complex<double> complex) {
+
+	std::complex<double> point((float)complex.real() / width - 1.5, (float)complex.imag() / height - 0.5);
+	std::complex<double> curr(0, 0);
+
+	int iter = 0; //mandelbrot require that a set remain bounded (by 2 here) after any number of iterations
+	const int MAX_ITERS = 30;
+	while (abs(curr) < 2 && iter < MAX_ITERS) {
+		curr = curr * curr + point;
+		iter++;
+	}
+
+	//cout << "iters: " << iter << endl;
+
+
+	Color color;
+
+	//cout << "iters: " << iter << endl;
+	int r = (255 * iter) / 33;
+	int g = (255 * iter) / 22;
+	int b = (255 * iter) / 11;
+	if (iter < 10) {
+		color.blue = 0;
+		color.green = 0;
+		color.red = 0;
+	} 
+	else {
+		color.blue = r;
+		color.green = g;
+		color.red = b;
+	} 
+
+	return color;
 }
