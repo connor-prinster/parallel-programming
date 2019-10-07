14d5b9ebe1d17237c2a9b27915e89c53b555d36e
Connor Prinster <connor.prinster@gmail.com>
2019-10-07 14:14:04 -0600

    legit just trying things
    maybe created a struct? who even knows
    
    things compile at the very least
    
    working on nastiness
    
    strting iwth strings instead of other stuff
    
    working on more stuff
    
    if we're going to be completely honest, I have no idea what's happening
    
    added a ternary
    
    added some things regarding a semicolon
    
    trying scatter gather
    
    troubleshooting
    
    working on things once again
    
    compiles, but is garbage
    
    added stuff, going to try Dakota's method
    
    should compile now?
    
    purely working on gather/scatter
    
    trying something new
    
    If we're being completely honest, I hav eno idea how this worked exactly
    
    will be trying to figure out the values now
    
    testing couting
    
    added .vscode to ignore and attempting more stuff
    
    more work, unfortunately
    
    small changes
    
    write the printing method
    
    added a weird plus ghting
    
    added a print function that might work
    
    added a brot
    
    new brot
    
    bot is doneish
    
    brot
    
    added another brot
    
    something is off, it goes off by an increasing 1 each 512
    
    may have it working now
    
    maybe did something good?
    
    yet another brot
    
    main now only prints the master timer. Idk why the first line is the only line that prints
    
    main is updated again
    
    retrying a lot of stuff
    
    working still, brand new idea
    
    something started working
    
    essentially done

--- paraBrot/main.cpp
+++ paraBrot/main.cpp
@@ -5,69 +5,100 @@
 #include <string>
 #include <complex>
 #include <fstream>
+#include <stddef.h>
 #include <mpi.h>
 
 #define MCW MPI_COMM_WORLD
 
 using namespace std;
 
-struct Color
-{
-	int red;
-	int green;
-	int blue;
-};
-
-int width = 512;
-int height = 512;
+const int WIDTH = 512;
+const int HEIGHT = 512;
+const int AREA = WIDTH * HEIGHT;
 const int MASTER = 0;
 
-Color generateValue(complex<double> complex);
+int generateValue(complex<double> complex);
+void printToFile(int rank, int* arr, int size);
 
 int main(int argc, char** argv) {
-	int rank, size;
+	// start on all the processes
+	auto beg = std::chrono::system_clock::now();
 
+	// set up the MPI stuff
+	int rank, numProc;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MCW, &rank);
-	MPI_Comm_size(MCW, &size);
+	MPI_Comm_size(MCW, &numProc);
 
-	// only run this on the master process
-	if (rank == MASTER) {
-		auto start = std::chrono::system_clock::now();
+	// if there aren't enough arguments, tell everyone they're wrong
+	if(rank == MASTER && argc < 4) {
+		printf("\nNot enough arguments. Requires 3 doubles as arguments\n");
+		return -1;
+	}
 
-		if (argc < 4) {
-			printf("\nNot enough arguments. Requires 3 doubles as arguments\n");
-			return -1;
+	if(rank == MASTER) {
+		ofstream ofs;
+		ofs.open("brot.ppm", std::ofstream::out | std::ofstream::trunc);
+		ofs.close();
+		printf("FILE WIPED\n\n");
+	}
+
+
+	MPI_Barrier(MCW);
+
+	// get the arguments
+	double data[4];
+	double r0 = atof(argv[1]);
+	double i0 = atof(argv[2]);
+	double r1 = atof(argv[3]);
+	double i1 = i0 + (r1 - r0);
+	data[0] = r0;
+	data[1] = i0;
+	data[2] = r1;
+	data[3] = i1;
+
+	// initialize interval data
+	int interval = 512/numProc;
+	int start = interval * rank;
+	int end = start + interval;
+
+	//int store[interval][WIDTH];
+	int store[interval * WIDTH];
+	int fakeEye = start;
+	int ctr = 0;
+
+	for(int i = 0; i < interval; i++) {
+		for(int j = 0; j < WIDTH; j++) {
+			std::complex<double> complex (
+				r0 + (j * (r1 - r0) / HEIGHT),
+				i0 + (fakeEye * (i1 - i0) / WIDTH)
+			);
+			int val = generateValue(complex);
+			store[ctr] = val;
+			ctr++;
 		}
-		double r0 = atof(argv[1]);
-		double i0 = atof(argv[2]);
-		double r1 = atof(argv[3]);
-		double i1 = i0 + (r1 - r0);
+		fakeEye++;
+	}
 
-		// check to see if you can even open the ppm file
-		ofstream mandel("brot.ppm");
-		if (!mandel.is_open()) {
-			printf("you failed, we'll get 'em next time\n\n");
-			return -1;
+	int dest = rank;
+	if(rank == 0) {
+		printToFile(rank, store, interval * WIDTH);
+//		MPI_Send(store, (interval * WIDTH), MPI_INT, 1, 0, MCW);
+		MPI_Send(&dest, 1, MPI_INT, 1, 0, MCW);
+	} else {
+//		MPI_Recv(store, (interval * WIDTH), MPI_INT, (dest - 1), 0, MCW, MPI_STATUS_IGNORE);
+		MPI_Recv(&dest, 1, MPI_INT, (dest - 1), 0, MCW, MPI_STATUS_IGNORE);
+		printToFile(rank, store, interval * WIDTH);
+		if(rank != (numProc - 1)) { // not the last process
+			MPI_Send(&dest, 1, MPI_INT, (rank + 1), 0, MCW);
 		}
+	}
 
-		// fill in the file
-		mandel << "P3\n" << width << " " << height << " 255\n"; // this particular line is for the header
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
-				std::complex<double> complex(
-					r0 + (j * (r1 - r0) / height),
-					i0 + (i * (i1 - i0) / width)
-				);
-				Color val = generateValue(complex);
-				mandel << val.red << " " << val.blue << " " << val.green << "\n";
-			}
-			mandel << "\n";
-		}
-		mandel.close();
 
+	// end the timing
+	if(rank == (numProc - 1)) {
 		auto end = std::chrono::system_clock::now();
-		std::chrono::duration<double> elapsed_seconds = end - start;
+		std::chrono::duration<double> elapsed_seconds = end - beg;
 		std::time_t end_time = std::chrono::system_clock::to_time_t(end);
 		std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 	}
@@ -76,9 +107,9 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-Color generateValue(complex<double> complex) {
+int generateValue(complex<double> complex) {
 
-	std::complex<double> point((float)complex.real() / width - 1.5, (float)complex.imag() / height - 0.5);
+	std::complex<double> point((float)complex.real() / WIDTH - 1.5, (float)complex.imag() / HEIGHT - 0.5);
 	std::complex<double> curr(0, 0);
 
 	int iter = 0; //mandelbrot require that a set remain bounded (by 2 here) after any number of iterations
@@ -88,25 +119,29 @@ Color generateValue(complex<double> complex) {
 		iter++;
 	}
 
-	//cout << "iters: " << iter << endl;
-
-
-	Color color;
-
-	//cout << "iters: " << iter << endl;
-	int r = (255 * iter) / 12;
-	int g = (255 * iter) / 21;
-	int b = (255 * iter) / 12;
-	if (iter < 10) {
-		color.blue = 0;
-		color.green = 0;
-		color.red = 0;
-	} 
-	else {
-		color.blue = r;
-		color.green = g;
-		color.red = b;
-	} 
-
-	return color;
+	return (iter < 10) ? 0 : 255;
 }
+
+void printToFile(int rank, int* arr, int size) {
+	// check to see if you can even open the ppm file
+	fstream mandel("brot.ppm", fstream::app);
+	if (!mandel.is_open()) {
+		printf("you failed, we'll open 'em next time\n\n");
+	}
+	if(rank == MASTER) {
+		mandel << "P3\n" << WIDTH << " " << HEIGHT << " 255\n"; // this particular line is for the header
+	}
+
+	cout << "Rank " << rank << " Printing to file" << endl;
+
+	for(int i = 0; i < size; i++) {
+		mandel << arr[i] << " " << 0 << " " << 0 << " ";;
+	}
+
+	mandel << "\n\n";
+
+	cout << "Rank " << rank <<  " Done Printing" << endl;
+
+	mandel.close();
+}
+
