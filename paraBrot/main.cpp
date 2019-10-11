9394fcf6decdc76e011a15a80c67c2ae1b6d5982
Connor Prinster <connor.prinster@gmail.com>
2019-10-11 17:40:24 -0600

    code review finished

--- paraBrot/main.cpp
+++ paraBrot/main.cpp
@@ -36,6 +36,7 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
+	// this is to wipe the file of any existing data
 	if(rank == MASTER) {
 		ofstream ofs;
 		ofs.open("brot.ppm", std::ofstream::out | std::ofstream::trunc);
@@ -47,26 +48,25 @@ int main(int argc, char** argv) {
 	MPI_Barrier(MCW);
 
 	// get the arguments
-	double data[4];
 	double r0 = atof(argv[1]);
 	double i0 = atof(argv[2]);
 	double r1 = atof(argv[3]);
 	double i1 = i0 + (r1 - r0);
-	data[0] = r0;
-	data[1] = i0;
-	data[2] = r1;
-	data[3] = i1;
 
 	// initialize interval data
 	int interval = 512/numProc;
 	int start = interval * rank;
 	int end = start + interval;
 
-	//int store[interval][WIDTH];
+	// the starting point of each process is determined here
 	int store[interval * WIDTH];
 	int fakeEye = start;
 	int ctr = 0;
 
+	// This nested loop goes through equal amounts of work for each process
+	// but the 'fakeEye' variable declared above and used below pretends
+	// to be the 'i' value that would have existed had this been a serial
+	// mandelbrot
 	for(int i = 0; i < interval; i++) {
 		for(int j = 0; j < WIDTH; j++) {
 			std::complex<double> complex (
@@ -83,24 +83,22 @@ int main(int argc, char** argv) {
 	int dest = rank;
 	if(rank == 0) {
 		printToFile(rank, store, interval * WIDTH);
-//		MPI_Send(store, (interval * WIDTH), MPI_INT, 1, 0, MCW);
 		MPI_Send(&dest, 1, MPI_INT, 1, 0, MCW);
 	} else {
-//		MPI_Recv(store, (interval * WIDTH), MPI_INT, (dest - 1), 0, MCW, MPI_STATUS_IGNORE);
 		MPI_Recv(&dest, 1, MPI_INT, (dest - 1), 0, MCW, MPI_STATUS_IGNORE);
 		printToFile(rank, store, interval * WIDTH);
-		if(rank != (numProc - 1)) { // not the last process
+		if(rank != (numProc - 1)) { // make sure it's not the last process
 			MPI_Send(&dest, 1, MPI_INT, (rank + 1), 0, MCW);
 		}
 	}
 
 
-	// end the timing
+	// end the timing on the last process holding data (the last one)
 	if(rank == (numProc - 1)) {
 		auto end = std::chrono::system_clock::now();
 		std::chrono::duration<double> elapsed_seconds = end - beg;
 		std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-		std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+		std::cout << "\n\nelapsed time: " << elapsed_seconds.count() << "s\n";
 	}
 
 	MPI_Finalize();
@@ -108,7 +106,6 @@ int main(int argc, char** argv) {
 }
 
 int generateValue(complex<double> complex) {
-
 	std::complex<double> point((float)complex.real() / WIDTH - 1.5, (float)complex.imag() / HEIGHT - 0.5);
 	std::complex<double> curr(0, 0);
 
@@ -128,18 +125,15 @@ void printToFile(int rank, int* arr, int size) {
 	if (!mandel.is_open()) {
 		printf("you failed, we'll open 'em next time\n\n");
 	}
+
 	if(rank == MASTER) {
 		mandel << "P3\n" << WIDTH << " " << HEIGHT << " 255\n"; // this particular line is for the header
 	}
-
 	cout << "Rank " << rank << " Printing to file" << endl;
-
 	for(int i = 0; i < size; i++) {
 		mandel << arr[i] << " " << 0 << " " << 0 << " ";;
 	}
-
 	mandel << "\n\n";
-
 	cout << "Rank " << rank <<  " Done Printing" << endl;
 
 	mandel.close();
