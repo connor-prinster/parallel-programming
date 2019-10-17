955d9775d235058ff3cb6d4bbfaa883819397589
Connor Prinster <connor.prinster@gmail.com>
2019-10-16 21:17:46 -0600

    the three methods are able to run now

--- globalSum/main.cpp
+++ globalSum/main.cpp
@@ -13,8 +13,10 @@ using namespace std;
 const int MASTER = 0;
 
 int generateRandom();
-void ringAroundTheNeighbor(int rank, int heldVal);
-void trustYourLeader(int rank, int heldVal);
+void masterSlave(int rank);
+void ringAroundTheNeighbor(int rank);
+void halfRing(int rank);
+void calculateEnd(auto start, string message);
 
 int main(int argc, char** argv) {
 	srand(time(NULL));
@@ -26,26 +28,9 @@ int main(int argc, char** argv) {
 	MPI_Comm_rank(MCW, &rank);
 	MPI_Comm_size(MCW, &size);
 
-	if(argc > 1 && atoi(argv[1]) < 2) {
-		choice = atoi(argv[1]);
-	} else {
-		if(rank == MASTER) {
-			printf("Invalid or no input. Choosing 0 as default\n\n\n");
-		}
-		choice = 0;
-	}
-
-	int heldVal = generateRandom();
-
-	trustYourLeader(rank, heldVal);
-	ringAroundTheNeighbor(rank, heldVal);
-
-	if(rank == MASTER) {
-		auto end = std::chrono::system_clock::now();
-		std::chrono::duration<double> elapsed_seconds = end - start;
-		std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-		std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
-	}
+	masterSlave(rank);
+	ringAroundTheNeighbor(rank);
+	halfRing(rank);
 
 	MPI_Finalize();
 	return 0;
@@ -55,11 +40,34 @@ int generateRandom() {
 	return (rand() % 100 + 1);
 }
 
-void ringAroundTheNeighbor(int rank, int heldVal) {
+void masterSlave(int rank) {
+	auto start = std::chrono::system_clock::now();
 
+	if(rank == MASTER) {
+		calculateEnd(start, "Master/Slave");
+	}
 }
 
-void trustYourLeader(int rank, int heldVal) {
+void ringAroundTheNeighbor(int rank) {
+	auto start = std::chrono::system_clock::now();
 
+	if(rank == MASTER) {
+		calculateEnd(start, "Running the Ring");
+	}
+}
+
+void halfRing(int rank) {
+	auto start = std::chrono::system_clock::now();
+
+	if(rank == MASTER) {
+		calculateEnd(start, "Half-ring");
+	}
+}
+
+void calculateEnd(auto start, string message) {
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+	std::cout << message << ": " << elapsed_seconds.count() << "s\n\n";
 }
 
